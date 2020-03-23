8d33985c8596a601e6a0251ec1aef7e1ca67dc4c
Jeff Huang <jeffshuang2@gmail.com>
2020-03-23 13:17:24 -0700

    Added images and made text styling changes.

--- src/components/AboutJumbotron/AboutJumbotron.js
+++ src/components/AboutJumbotron/AboutJumbotron.js
@@ -11,13 +11,13 @@ function AboutJumbotron() {
                     {/* <div className="headerLineShort"></div> */}
                     {/* <p id="aboutHeaderSubtext" className="centerText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut.</p> */}
                     <div id="aboutJumbotronContent">
-                        <p className="aboutHeaderParagraphText">My background stems from a mixture of marketing and UX/UI design. After graduating from the University of Louisville with a degree in marketing, I discovered a newfound interest in designing user experiences. It was soon after discovering UX that I realized I enjoyed creating these user experiences more than I did designing them.</p>
+                        <p className="aboutHeaderParagraphText">My background stems from a diverse combination of marketing and UX/UI design. After graduating from the University of Louisville in 2017 with a degree in marketing, I discovered a newfound interest in UX design. It was soon after discovering UX that I realized I enjoyed creating these user experiences a bit more than I did designing them.</p>
 
                         
-                        <p className="aboutHeaderParagraphText">I enrolled in the University of Washington's full-stack web development coding bootcamp. I can honestly say I never enjoyed learning more than I did while taking this course. As someone whose life has revolved around computers and technology their entire life, I fell in love with coding immediately! </p>
+                        <p className="aboutHeaderParagraphText">I enrolled in the University of Washington's full-stack web development coding bootcamp in August 2019. I can honestly say I've never enjoyed learning more than I did while taking this course. As someone whose life has revolved around computers and technology, I fell in love with coding immediately! I graduated from bootcamp in February 2020 and am now looking for full-time employment.</p>
                         
                         
-                        <p className="aboutHeaderParagraphText">I specialize in front end development with React JS, but I also have experience developing full-stack MERN applications. However, like I said, I love learning so I'm constantly trying to learn new innovative technologies in the programming world.</p>
+                        <p className="aboutHeaderParagraphText">I specialize in front end development with React JS, but I also have experience developing full-stack MERN applications. However, like I said, I enjoy learning so I'm constantly dabbling with new innovative technologies in the programming world.</p>
                     </div>
                     <img id="profilePic" src={profilePic} alt="Profile"></img>
                 </div>
--- src/components/HomeJumbotron/HomeJumbotron.js
+++ src/components/HomeJumbotron/HomeJumbotron.js
@@ -9,8 +9,8 @@ function HomeJumbotron() {
                 <div className="container">
                     <div id="jumbotronContent">
                         <h2 id="jumbotronHeader">Hi, I'm Jeff.</h2>
-                        <h3 id="headerSubtext">I'm a front end web developer based in Seattle, Washington.</h3>
-                        <p id="headerParagraphText">I enjoy creating delightful and beautiful user experiences with React. When I'm not coding, you can find me playing soccer, video games, or hiking in the Cascades.</p>
+                        <h3 id="headerSubtext">I'm a web developer based in Seattle, Washington.</h3>
+                        <p id="headerParagraphText">I like creating enjoyable user experiences with React. While I'm not coding, you can find me playing soccer, video games, or hiking in the Cascades.</p>
                     </div>
                     <img id="profilePic" src={profilePic} alt="Profile"></img>
                 </div>
