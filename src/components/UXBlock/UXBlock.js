ac34e4ab1038d59783d84693cb6c2ecc2eb840b4
Jeff Huang <jeffshuang2@gmail.com>
2020-04-07 16:09:20 -0700

    Added links to each project on the homepage.

--- src/components/UXBlock/UXBlock.js
+++ src/components/UXBlock/UXBlock.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import Placeholder from "../../utils/assets/images/image-placeholder.png"
 import wanderWeb from "../../utils/assets/images/wander-web-mockup.PNG"
 import wanderApp from "../../utils/assets/images/wander-app-mockup.png"
 import craft from "../../utils/assets/images/craft-mockup.jpg"
@@ -14,15 +13,21 @@ class CodeBlock extends Component {
                     <h3 className="bodyHeader">UX/UI Design</h3>
                 </div>
                 <div className="cardWrapper" id="uxWrapper">
-                    <div className="card homeCard">
-                        <img className="cardImage" src={wanderWeb} alt="Project One" />
-                    </div>
-                    <div className="card homeCard">
-                        <img className="cardImage" src={wanderApp} alt="Project Two" />
-                    </div>
-                    <div className="card homeCard">
-                        <img className="cardImage" src={craft} alt="Project Three" />
-                    </div>
+                    <a href="https://jeffhuangux.com/wander-webpage" target="_blank" rel="noopener noreferrer">
+                        <div className="card homeCard">
+                            <img className="cardImage" src={wanderWeb} alt="Project One: Wander Landing Page" />
+                        </div>
+                    </a>
+                    <a href="https://jeffhuangux.com/wander" target="_blank" rel="noopener noreferrer">
+                        <div className="card homeCard">
+                            <img className="cardImage" src={wanderApp} alt="Project Two: Wander App" />
+                        </div>
+                    </a>
+                    <a href="https://jeffhuangux.com/craft" target="_blank" rel="noopener noreferrer">
+                        <div className="card homeCard">
+                            <img className="cardImage" src={craft} alt="Project Three: Craft App" />
+                        </div>
+                    </a>
                 </div>
             </div>
         )
