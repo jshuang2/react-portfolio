04d2ae294927a2a1e5e01a955a4311a367b694b2
Jeff Huang <jeffshuang2@gmail.com>
2020-04-07 16:24:43 -0700

    Added links to homepage for three more projects.

--- src/components/CodeBlock/CodeBlock.js
+++ src/components/CodeBlock/CodeBlock.js
@@ -12,15 +12,21 @@ class CodeBlock extends Component {
                             <h3 className="bodyHeader">Code</h3>
                         </div>
                         <div className="cardWrapper">
-                            <div className="card homeCard">
-                                <img className="cardImage" src={context[0].image} alt="Project One" />
-                            </div>
-                            <div className="card homeCard">
-                                <img className="cardImage" src={context[1].image} alt="Project Two" />
-                            </div>
-                            <div className="card homeCard">
-                                <img className="cardImage" src={context[3].image} alt="Project Three" />
-                            </div>
+                            <a href="https://github.com/jshuang2/react-google-books-search" target="_blank" rel="noopener noreferrer">
+                                <div className="card homeCard">
+                                    <img className="cardImage" src={context[0].image} alt="Project One: Google Books Search" />
+                                </div>
+                            </a>
+                            <a href="https://github.com/jshuang2/react-clicky-game" target="_blank" rel="noopener noreferrer">
+                                <div className="card homeCard">
+                                    <img className="cardImage" src={context[1].image} alt="Project Two: Game of Thrones Memory" />
+                                </div>
+                            </a>
+                            <a href="https://github.com/jshuang2/MongoWebScraper" target="_blank" rel="noopener noreferrer">
+                                <div className="card homeCard">
+                                    <img className="cardImage" src={context[3].image} alt="Project Three: Mongo Web Scraper" />
+                                </div>
+                            </a>
                         </div>
                     </div>
                 )}
