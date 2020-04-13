641e9b0793ff00bd1b9f63ed59c0aa5038081723
Jeff Huang <jeffshuang2@gmail.com>
2020-04-13 14:35:15 -0700

    Changed some page structure and text.

--- src/components/HomeJumbotron/HomeJumbotron.js
+++ src/components/HomeJumbotron/HomeJumbotron.js
@@ -9,8 +9,8 @@ function HomeJumbotron() {
                 <div className="container">
                     <div id="jumbotronContent">
                         <h2 id="jumbotronHeader">Hi, I'm Jeff.</h2>
-                        <h3 id="headerSubtext">I'm a web developer based in Seattle, Washington.</h3>
-                        <p id="headerParagraphText">I like creating enjoyable user experiences with React. While I'm not coding, you can find me playing soccer, video games, or hiking in the Cascades.</p>
+                        <h3 id="headerSubtext">I'm a full-stack web developer based in Seattle, Washington.</h3>
+                        <p id="headerParagraphText">I create enjoyable user experiences with React. While I'm not coding, you can find me playing soccer, video games, or hiking the mountains in the Pacific Northwest.</p>
                     </div>
                     <img id="profilePic" src={profilePic} alt="Profile"></img>
                 </div>
