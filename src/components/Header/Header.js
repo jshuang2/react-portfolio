bc0b01408be2a23ab54d3c0f8956d720ac4ba3f5
Jeff Huang <jeffshuang2@gmail.com>
2020-04-29 17:40:49 -0700

    Made the code more efficient for changing the css dynamically in the header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,37 +6,41 @@ import { Link } from "react-router-dom"
 
 class Header extends Component {
 
-   state = {
-       isOpen: "false",
-       textColor: "white",
-       backgroundColor: "none"
-   }
+    state = {
+        isMenuVisible: false
+    }
 
-   onClickHandler = () => {
+    toggleMenu = () => {
+        this.setState(prevState => (
+            { isMenuVisible: !prevState.isMenuVisible }
+        ));
+    }
 
-    // const navMenu = document.getElementById("navbarNavAltMarkup");
-    // const navItem = document.getElementsByClassName("nav-item");
+    // onClickHandler = () => {
 
-       if (this.state.isOpen === "false") {
-            // navMenu.style.backgroundColor = "#f9f9f9";
-            // navMenu.style.width = "20%";
-            // console.log(navItem);
+    //     // const navMenu = document.getElementById("navbarNavAltMarkup");
+    //     // const navItem = document.getElementsByClassName("nav-item");
 
-            this.setState({
-                isOpen: "true",
-                textColor: "black",
-                backgroundColor: "red"
-            })
-       }
+    //     if (this.state.isOpen === "false") {
+    //         // navMenu.style.backgroundColor = "#f9f9f9";
+    //         // navMenu.style.width = "20%";
+    //         // console.log(navItem);
 
-       else {
-            this.setState({
-                isOpen: "false",
-                textColor: "white",
-                backgroundColor: "none"
-            })
-       }
-   }
+    //         this.setState({
+    //             isOpen: "true",
+    //             textColor: "black",
+    //             backgroundColor: "red"
+    //         })
+    //     }
+
+    //     else {
+    //         this.setState({
+    //             isOpen: "false",
+    //             textColor: "white",
+    //             backgroundColor: "none"
+    //         })
+    //     }
+    // }
 
     render() {
         return (<div>
@@ -44,23 +48,23 @@ class Header extends Component {
                 <div className="container">
                     <Link className="navbar-brand" to="/">Jeff Huang</Link>
 
-                    <button className="navbar-toggler" id="hamburger" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded={this.state.isOpen} onClick={this.onClickHandler} aria-label="Toggle navigation">
+                    <button className="navbar-toggler" id="hamburger" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded={this.state.isOpen} onClick={this.toggleMenu} aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse" style={{backgroundColor: this.state.backgroundColor}}id="navbarNavAltMarkup">
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <ul className="navbar-nav ml-auto">
-                            <li className="nav-item nav-link pageLink" style={{color: this.state.textColor}}><Link to="/about">About</Link></li>
-                            <li className="nav-item nav-link pageLink" style={{color: this.state.textColor}}><Link to="/portfolio">Portfolio</Link></li>
-                            <li><a className="nav-item nav-link pageLink" href="https://drive.google.com/file/d/1Vp_46WkDn_8eggbRxS85PAPnEkPKm-SU/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
+                            <li className={`nav-item nav-link pageLink ${this.state.isMenuVisible ? "openMenu" : ""}`}><Link to="/about">About</Link></li>
+                            <li className={`nav-item nav-link pageLink ${this.state.isMenuVisible ? "openMenu" : ""}`}><Link to="/portfolio">Portfolio</Link></li>
+                            <li><a className={`nav-item nav-link pageLink ${this.state.isMenuVisible ? "openMenu" : ""}`} href="https://drive.google.com/file/d/1Vp_46WkDn_8eggbRxS85PAPnEkPKm-SU/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
                             </li>
                         </ul>
 
 
                         <ul className="navbar-nav socialMediaList">
-                            <li className="nav-item nav-link"><a href="https://github.com/jshuang2" target="_blank" rel="noopener noreferrer"><i className="fab fa-github socialMedia"></i></a></li>
-                            <li className="nav-item nav-link"><a href="mailto:jeffshuang2@gmail.com"><i className="fas fa-envelope socialMedia"></i></a></li>
-                            <li className="nav-item nav-link"><a href="https://www.linkedin.com/in/jeffshuang/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in socialMedia"></i></a></li>
+                            <li className={`nav-item nav-link ${this.state.isMenuVisible ? "openMenu" : ""}`}><a href="https://github.com/jshuang2" target="_blank" rel="noopener noreferrer"><i className="fab fa-github socialMedia"></i></a></li>
+                            <li className={`nav-item nav-link ${this.state.isMenuVisible ? "openMenu" : ""}`}><a href="mailto:jeffshuang2@gmail.com"><i className="fas fa-envelope socialMedia"></i></a></li>
+                            <li className={`nav-item nav-link ${this.state.isMenuVisible ? "openMenu" : ""}`}><a href="https://www.linkedin.com/in/jeffshuang/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in socialMedia"></i></a></li>
                         </ul>
 
 
