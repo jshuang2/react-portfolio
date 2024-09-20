bc822088a017f54a4a311ebb1fb5a36d7fd09b7c
Jeff Huang <jeffshuang2@gmail.com>
2024-09-19 21:05:27 -0700

    Updated about information and resized profile pic.

--- src/components/AboutJumbotron/AboutJumbotron.js
+++ src/components/AboutJumbotron/AboutJumbotron.js
@@ -14,13 +14,13 @@ function AboutJumbotron() {
                     <h2 id="aboutMeHeader">Who am I?</h2>
                     </div>
                     <div id="aboutJumbotronContent">
-                        <p className="aboutHeaderParagraphText">My background stems from a unique combination of marketing and UX/UI design. After graduating from the University of Louisville in 2017 with a degree in marketing, I discovered a newfound interest in UX design. It was soon after discovering UX that I realized I enjoyed creating these user experiences a bit more than I did designing them.</p>
+                        <p className="aboutHeaderParagraphText">Originally from a marketing and sales background, it wasn't until 2019 that I discovered a genuine passion for coding and building software. I realized that I wasn't finding any joy in persuading people and companies to buy products that I myself did not believe in. So, I learned to code and never looked back. Now, my goal is to use this newfound skill as a tool to create software solutions I believe in and that truly help people in their everyday lives.</p>
 
                         
-                        <p className="aboutHeaderParagraphText">I enrolled in the University of Washington's full-stack web development coding bootcamp in August 2019. I can honestly say I've never enjoyed learning more than I did while taking this course. As someone whose life has revolved around computers and technology, I fell in love with coding immediately! I graduated from bootcamp in February 2020 and am now looking for full-time employment.</p>
+                        <p className="aboutHeaderParagraphText">In 2019, I dove head-first into coding and learned full-stack web-development at University of Washington's coding bootcamp. After that, I found a role as an Associate Software Engineer at Infosys where I became proficient in Java and automation testing. Now, I've decided to pursue a Master of Science in Computer Science degree at Northeastern University here in Seattle where I hope to hone my skills in back-end development and building scalable systems. I'm currently seeking a 2025 software engineer internship!</p>
                         
                         
-                        <p className="aboutHeaderParagraphText">I specialize in front end development with React JS, but I also have experience developing full-stack MERN applications. However, I'm constantly dabbling with new innovative technologies in the programming world.</p>
+                        {/* <p className="aboutHeaderParagraphText">I specialize in back-end development using Java and Python. I also have experience with full-stack development using React. However, I'm constantly dabbling with new innovative technologies in the programming world. I'm currently seeking a 2025 internship as a software engineer!</p> */}
                     </div>
                     <div className="aboutImagesContainer">
                     <img id="montana" className="aboutPic" src={Montana} alt="Montana"></img>
